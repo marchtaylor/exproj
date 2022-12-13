8dd1c7e3b0326fcef1d433a883d6ba5066a06dc3
Marc Taylor <marchtaylor@gmail.com>
2022-12-13 08:53:13 +0100

    initial

---
+++ R/01_script.R
@@ -0,0 +1,29 @@
+
+# 0. required packages -------------------------------------------------------
+library(MASS)
+
+
+# 1. plots -------------------------------------------------------------------
+
+png("output/volcano.png", width = 5, height = 5, units = "in", res = 400)
+  image(volcano)
+dev.off()
+
+dat <- MASS::mvrnorm(n = 1000, mu = c(0,0), Sigma = array(c(1,0.7,1,0.7), dim = c(2,2)))
+tiff("output/mvrnorm.tiff", width = 5, height = 5, units = "in", res = 400, compression = "lzw")
+  plot(dat)
+dev.off()
+
+png("output/mvrnorm.png", width = 5, height = 5, units = "in", res = 400)
+  plot(dat)
+dev.off()
+
+
+# 2. tables ---------------------------------------------------------------
+iris2 <- read.csv(file = "data/iris.csv", row.names = 1)
+P <- prcomp(as.matrix(iris[,1:4]))
+iris3 <- cbind(iris2, P$x[,1:2])
+
+save(iris3, file = "output/iris3.Rdata")
+
+
