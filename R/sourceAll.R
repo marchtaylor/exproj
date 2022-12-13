48fda58941e477b9ea1080427a7c0318042a751b
Marc Taylor <marchtaylor@gmail.com>
2022-12-13 10:30:41 +0100

    sourceAll added

---
+++ R/sourceAll.R
@@ -0,0 +1,10 @@
+
+# source setup
+source("R/00_setup.R")
+source("R/01_script.R")
+
+# library(rmarkdown)
+# rmarkdown::render(
+#   input = "/docs/supplMat.Rmd",
+#   output_file = "/docs/supplMat.docx"
+# )
